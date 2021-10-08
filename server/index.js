e67a1f1f7254d2f211ebf3654ddd3f925bc7b3bd
Zhen Yang <45318296+yangzhen1@users.noreply.github.com>
2021-10-08 08:10:04 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
-app.set('port', (process.env.PORT || 80))
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
